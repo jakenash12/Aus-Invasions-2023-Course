66db3ef456ee496c5942f147ece85eea49c3a434
Beatrice Bock <bmb646@nau.edu>
2024-10-30 14:50:44 -0700

    working onbiolog regression

--- biolog_regressions.R
+++ biolog_regressions.R
@@ -3,20 +3,30 @@
 #Trying some regressions to begin with
 
 library(ggplot2)
+library(mvabund)
 
 ds <- read.csv("Aus23_CNP_pooled_biolog.csv")
 
 
 # Gadgil effect -----------------------------------------------------------
-
+#How does ECM affect litter decomp in the two types of soils?
 
 mod <- lm(N.Acetyl.D.Glucosamine ~ ECM_richness_soil + perc_N + ergosterol + TreeSpecies, ds)
 summary(mod)
 
 mod <- lm(ergosterol ~ ECM_abund_soil + perc_N + ergosterol + TreeSpecies, ds)
 summary(mod)
-#interesting...
+#interesting... mpisture should be in this too
+
+# many <- manylm(N.Acetyl.D.Glucosamine~.,data=ds)
+# many
+
+
 
 #link who they are and what they do
 
 #manylm. Try this function
+
+#Do some sort of AIC-based model selection with all predictors
+#Response variables; Total CNP, decomp, 
+#Decomp 
