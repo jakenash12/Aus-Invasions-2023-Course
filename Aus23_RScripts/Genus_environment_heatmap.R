a6e166316de164e66146e21c0dccb3e28f3f8e2b
Corinne Vietorisz <cviet@bu.edu>
2025-01-07 20:34:12 -0500

    Update genus heatmap and DBH plots

--- Aus23_RScripts/Genus_environment_heatmap.R
+++ Aus23_RScripts/Genus_environment_heatmap.R
@@ -3,13 +3,15 @@
 
 library(DescTools)
 library(tidyverse)
+library(tidyr)
+library(ggplot2)
 
 # read in full data
 fulldat <- read.csv("Aus-Invasions-2023-Course/Merged_data/Aus23_allData_19Nov24.csv", row.names=1)
 
 # read in genus-level pooled OTU tables
-otu.genus.16S <- read.csv("Aus-Invasions-2023-Course/Aus23_16S_Metabarcoding/OTUTables/OtuMat16S_rel_byGenus_pooled.csv", row.names=1)
-otu.genus.its <- read.csv("Aus-Invasions-2023-Course/Aus23_ITS_Metabarcoding/OTUTables/OtuMatITS_rel_byGenus_pooled.csv", row.names=1)
+otu.genus.16S <- read.csv("Aus-Invasions-2023-Course/Aus23_16S_Metabarcoding/OTUTables/OtuMat16S_rare_byGenus_pooled.csv", row.names=1)
+otu.genus.its <- read.csv("Aus-Invasions-2023-Course/Aus23_ITS_Metabarcoding/OTUTables/OtuMatITS_rare_byGenus_pooled.csv", row.names=1)
 # add tree ID
 otu.genus.16S <- otu.genus.16S %>%
   rownames_to_column("SampleID") %>%
@@ -32,10 +34,6 @@ fun.soil.gen.names <- c("treeName", "Phialocephala", "Thelephora", "Athelopsis",
 bac.root.gen <- root.16s[bac.root.gen.names]
 fun.soil.gen <- soil.its[fun.soil.gen.names]
 
-# merge each OTU table with environmental data
-
-
-
 # pull out significant environmental variables and differentially abundant genera
 sigvars <- c("treeName", "Tween_40_BiologDay5", "L.Serine_BiologDay5", "Glycyl.L.Glutamic_Acid_BiologDay5",
              "i.Erythritol_BiologDay5", "Putrescine_BiologDay5", "L.Asparagine_BiologDay5", 
@@ -44,7 +42,116 @@ sigvars <- c("treeName", "Tween_40_BiologDay5", "L.Serine_BiologDay5", "Glycyl.L
              "Bac_Shannon_soil")
 sigdat <- fulldat[sigvars]
 
+# merge each OTU table with environmental data
+bac.root.sig <- merge(bac.root.gen, sigdat, by = "treeName")
+fun.soil.sig <- merge(fun.soil.gen, sigdat, by = "treeName")
 
+##################### Run correlations ##################### 
+library(Hmisc)
 
+#### Bacterial roots #####
 
+# Get correlation value (R) and P value (P)
+bac.root.r <- rcorr(as.matrix(bac.root.sig[2:ncol(bac.root.sig)]))$r
+bac.root.p <- rcorr(as.matrix(bac.root.sig[2:ncol(bac.root.sig)]))$P
+
+# subset for just the relationships we want
+bac.root.r.format <- bac.root.r %>% 
+  as.data.frame(bac.root.r)  %>%
+  select(1:(length(bac.root.gen.names)-1)) %>% 
+  slice(-(1:(length(bac.root.gen.names)-1)))
+rownames(bac.root.r.format) <- sigvars[2:17]
+
+bac.root.p.format <- bac.root.p %>% 
+  as.data.frame(bac.root.p)  %>%
+  select(1:(length(bac.root.gen.names)-1)) %>% 
+  slice(-(1:(length(bac.root.gen.names)-1)))
+rownames(bac.root.p.format) <- sigvars[2:17]
+
+# pivot df longer
+bac.root_long <- bac.root.r.format %>%
+  rownames_to_column("Variable") %>%
+  pivot_longer(cols = -Variable, names_to = "Genus", values_to = "Corr_value")
+
+# add p-values
+bac.rootP_long <- bac.root.p.format %>%
+  rownames_to_column("Variable") %>%
+  pivot_longer(cols = -Variable, names_to = "Genus", values_to = "P_value")
+# generate stars to indicate significance
+bac.rootP_long$P_star <- " "
+for(i in 1:nrow(bac.rootP_long)){
+  val <- bac.rootP_long$P_value[i]
+  if(val < 0.001){
+    bac.rootP_long$P_star[i] <- "***"
+  } else if(val < 0.01){
+    bac.rootP_long$P_star[i] <- "**"
+  } else if(val < 0.05){
+    bac.rootP_long$P_star[i] <- "*"
+  }
+}
+
+bac.root_long$P_star <- bac.rootP_long$P_star
+
+# make heatmap
+ggplot(bac.root_long, aes(x = Genus, y = Variable, fill = Corr_value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "dodgerblue4", mid = "white", high = "green4", midpoint = 0) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(fill = "Correlation") +
+  coord_fixed() + 
+  geom_text(aes(label = P_star), size = 7)
+
+#### Fungal soil #####
+
+# Get correlation value (R) and P value (P)
+fun.soil.r <- rcorr(as.matrix(fun.soil.sig[2:ncol(fun.soil.sig)]))$r
+fun.soil.p <- rcorr(as.matrix(fun.soil.sig[2:ncol(fun.soil.sig)]))$P
+
+# subset for just the relationships we want
+fun.soil.r.format <- fun.soil.r %>% 
+  as.data.frame(fun.soil.r)  %>%
+  select(1:(length(fun.soil.gen.names)-1)) %>%
+  slice(-(1:length(fun.soil.gen.names)-1))
+rownames(fun.soil.r.format) <- sigvars[2:17]
+
+fun.soil.p.format <- fun.soil.p %>% 
+  as.data.frame(fun.soil.p)  %>%
+  select(1:(length(fun.soil.gen.names)-1)) %>%
+  slice(-(1:length(fun.soil.gen.names)-1))
+rownames(fun.soil.p.format) <- sigvars[2:17]
+
+# pivot df longer
+fun.soil_long <- fun.soil.r.format %>%
+  rownames_to_column("Variable") %>%
+  pivot_longer(cols = -Variable, names_to = "Genus", values_to = "Corr_value")
+
+# add p-values
+fun.soilP_long <- fun.soil.p.format %>%
+  rownames_to_column("Variable") %>%
+  pivot_longer(cols = -Variable, names_to = "Genus", values_to = "P_value")
+# generate stars to indicate significance
+fun.soilP_long$P_star <- " "
+for(i in 1:nrow(fun.soilP_long)){
+  val <- fun.soilP_long$P_value[i]
+  if(val < 0.001){
+    fun.soilP_long$P_star[i] <- "***"
+  } else if(val < 0.01){
+    fun.soilP_long$P_star[i] <- "**"
+  } else if(val < 0.05){
+    fun.soilP_long$P_star[i] <- "*"
+  }
+}
+
+fun.soil_long$P_star <- fun.soilP_long$P_star
+
+# make heatmap
+ggplot(fun.soil_long, aes(x = Genus, y = Variable, fill = Corr_value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "dodgerblue4", mid = "white", high = "green4", midpoint = 0) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(fill = "Correlation") +
+  coord_fixed() + 
+  geom_text(aes(label = P_star), size = 7)
 
--- DBH_regressions/dbh_significant_variables_regressions_plots_v2.R
+++ DBH_regressions/dbh_significant_variables_regressions_plots_v2.R
@@ -1,19 +1,25 @@
+# Regressions of variables that differ between Euc vs Pine by tree DBH
+# Graphs started Dec 2024 by Caitlín Dagg
+# Stats Jan 2025 by Corinne V
+
+
 library(dplyr)
 library(reshape2)
 library(ggplot2)
 library(tidyr)
 library(ggpmisc)
+library(car)
 install.packages("ggpmisc")
 
 
-Aus23_allData_19Nov24 <- read.csv("Merged_data/Aus23_allData_19Nov24.csv") %>% select(-X)
-variables.signif.take2<-c("Tween_40_BiologDay5", "L.Serine_BiologDay5", "Glycyl.L.Glutamic_Acid_BiologDay5",
-                          "i.Erythritol_BiologDay5", "Putrescine_BiologDay5", "L.Asparagine_BiologDay5", 
-                          "pine_litter_prop", "Euc_leafLitter_percP", "Euc_leafLitter_totP",              
-                          "Litter_OLayer_ergosterol", "Litter_avg_ergosterol","euc_litter_prop", "Path_abund_root", "ergosterol","soil_moisture",
-                          "Bac_Shannon_soil")
+Aus23_allData_19Nov24 <- read.csv("Aus-Invasions-2023-Course/Merged_data/Aus23_allData_19Nov24.csv") %>% select(-X)
+variables.signif.take2<-c("Bac_Shannon_soil","Path_abund_root", "ergosterol", "Tween_40_BiologDay5", "L.Serine_BiologDay5", 
+                          "Glycyl.L.Glutamic_Acid_BiologDay5",
+                          "i.Erythritol_BiologDay5", "Putrescine_BiologDay5", "L.Asparagine_BiologDay5", "soil_moisture",
+                          "pine_litter_prop", "euc_litter_prop", "Euc_leafLitter_percP",           
+                          "Litter_OLayer_ergosterol", "Litter_avg_ergosterol")
 
-
+############################# PLOTS ############################# 
 
 # Filter and pivot data to long format
 data_long_signif <- Aus23_allData_19Nov24 %>%
@@ -21,30 +27,29 @@ data_long_signif <- Aus23_allData_19Nov24 %>%
   pivot_longer(
     cols = -c(dbh_cm, TreeSpecies), 
     names_to = "Response", 
-    values_to = "Value"
-  )
+    values_to = "Value")
+data_long_signif$Response <- factor(data_long_signif$Response, levels = variables.signif.take2)
 
 # Plot regressions for each variable against dbh_cm
-ggplot(data_long_signif, aes(x = dbh_cm, y = Value, color = TreeSpecies)) +
+ggplot(data_long_signif, aes(x = dbh_cm, y = Value, color = factor(TreeSpecies, c("Pine", "Eucalyptus")))) +
   geom_point(size = 2, alpha = 0.7) +  # Scatterplot points
-  geom_smooth(method = "lm", se = FALSE, linetype = "dashed") +  # Linear regression line
- # geom_smooth(method = "loess", se = TRUE, linetype = "solid", color = "black") +  # LOESS trend line with confidence intervals
-  stat_poly_eq(aes(label = paste(..rr.label.., ..p.value.label.., sep = "~~~")), # plot R2 and p value on the graph
-               label.x = "right", label.y = "top",
-               parse = TRUE, size = 3) +
+  geom_smooth(method = "lm", se = TRUE, alpha = 0.15, aes(linetype = TreeSpecies)) +  # Linear regression line
   facet_wrap(~ Response, scales = "free_y") +  # Facet by response variable
-  labs(title = "Regression of Significant Variables Against DBH",
-       x = "DBH (cm)",
+  labs(x = "DBH (cm)",
        y = "Response Variable Value") +
+  scale_color_manual(values = c("#000DCC", "#FF9900")) +
   theme_minimal() +
   theme(
     legend.position = "bottom",
+    panel.spacing.x = unit(2, "lines"),
     strip.text = element_text(size = 10, face = "bold"),  # Facet label formatting
-    axis.title = element_text(size = 12)
+    axis.title = element_text(size = 12),
+    axis.text = element_text(colour="black"),
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(),
+    axis.line = element_line(colour = "black")
   )
 
-ggsave("DBH_regressions/Regression_Plots_DBH_vs_Signif_Variables_Faceted_Stats.png", width = 14, height = 8, dpi = 300)
-
+ggsave("Aus-Invasions-2023-Course/DBH_regressions/Regression_Plots_DBH_vs_Signif_Variables_Faceted_Stats_v2.png", width = 14, height = 8, dpi = 300)
 
 
 # Loop through each significant variable and plot separately
@@ -71,5 +76,88 @@ for (var in variables.signif.take2) {
   # Save the plot as a PNG file
   ggsave(paste0( "DBH_vs_", var, ".png"), plot = p, width = 8, height = 6, dpi = 300)
 }
+
+############################# STATS ############################# 
+hist(Aus23_allData_19Nov24$dbh_cm)
+
+tween.lm <- lm(Tween_40_BiologDay5 ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(tween.lm, test.statistic = "F") 
+plot(tween.lm) # dbh, tree
+
+moisture.lm <- lm(soil_moisture ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(moisture.lm, test.statistic = "F") 
+plot(moisture.lm) # dbh, tree, int
+
+Lserine.lm <- lm(L.Serine_BiologDay5 ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(Lserine.lm, test.statistic = "F")
+plot(Lserine.lm) # tree marginal
+
+glycyl.lm <- lm(Glycyl.L.Glutamic_Acid_BiologDay5 ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(glycyl.lm, test.statistic = "F")
+plot(glycyl.lm) # tree
+# residuals are eh...
+
+iEry.lm <- lm(i.Erythritol_BiologDay5 ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(iEry.lm, test.statistic = "F")
+plot(iEry.lm) # tree
+
+putre.lm <- lm(Putrescine_BiologDay5 ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(putre.lm, test.statistic = "F")
+plot(putre.lm) # tree
+
+LAsp.lm <- lm(L.Asparagine_BiologDay5 ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(LAsp.lm, test.statistic = "F")
+plot(LAsp.lm) # tree
+
+pinelit.lm <- lm(pine_litter_prop ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(pinelit.lm, test.statistic = "F")
+plot(pinelit.lm) # tree, dbh
+
+eucP.lm <- lm(Euc_leafLitter_percP ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(eucP.lm, test.statistic = "F")
+plot(eucP.lm) # tree
+
+Oergosterol.lm <- lm(Litter_OLayer_ergosterol ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(Oergosterol.lm, test.statistic = "F")
+plot(Oergosterol.lm) # tree
+
+avgLitErg.lm <- lm(Litter_avg_ergosterol ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(avgLitErg.lm, test.statistic = "F")
+plot(avgLitErg.lm) # tree
+
+euclit.lm <- lm(euc_litter_prop ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(euclit.lm, test.statistic = "F")
+plot(euclit.lm) # dbh, tree
+
+rootpath.lm <- lm(Path_abund_root ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(rootpath.lm, test.statistic = "F")
+plot(rootpath.lm) # tree
+# residuals iffy
+
+soilErg.lm <- lm(ergosterol ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(soilErg.lm, test.statistic = "F")
+plot(soilErg.lm) # tree
+
+bacshannon.lm <- lm(Bac_Shannon_soil ~ dbh_cm*TreeSpecies, data = Aus23_allData_19Nov24)
+Anova(bacshannon.lm, test.statistic = "F")
+plot(bacshannon.lm) # dbh, tree
+
+
+
+
+# specify y axis labels for each variable
+Ylabels <- c("Tween40 metabolism", "L-Serine metabolism", "Glycyl-L-Glutamic Acid metabolism",
+             "i-Erythritol metabolism", "Putrescine metabolism", "L-Asparagine metabolism", 
+             "Proportion pine litter", "Proportion eucalypt litter", "Eucalpyt leaf litter %P",           
+             "Litter O layer ergosterol", "All litter layer ergosterol", "Soil ergosterol content", "Root fungal pathogen relative abundance", "Soil moisture",
+             "Soil bacterial Shannon diversity")
+
+variables.signif.take2<-c("Tween_40_BiologDay5", "L.Serine_BiologDay5", "Glycyl.L.Glutamic_Acid_BiologDay5",
+                          "i.Erythritol_BiologDay5", "Putrescine_BiologDay5", "L.Asparagine_BiologDay5", 
+                          "pine_litter_prop", "Euc_leafLitter_percP", "Euc_leafLitter_totP",              
+                          "Litter_OLayer_ergosterol", "Litter_avg_ergosterol","euc_litter_prop", "Path_abund_root", 
+                          "ergosterol","soil_moisture",
+                          "Bac_Shannon_soil")
+
 
 
